358db4f91f3c351d11a1098b74009b3ae3ee0ccc
Nils <78668364+Suchti18@users.noreply.github.com>
2025-07-19 02:20:43 +0200

    - Moved GUI code to view.java - Added link for java 8 32 bit version to README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # explorer
 Windows 10 Explorer for Windows XP
+
+## Java for Windows XP
+Download "jre-8u51-windows-i586.exe" to run the app on Windows xp:
+https://archive.org/download/Java_8_update_51
--- src/main/java/de/nils/explorer/view/view.java
+++ src/main/java/de/nils/explorer/view/view.java
@@ -1,5 +1,31 @@
 package de.nils.explorer.view;
 
+import javax.swing.*;
+import java.awt.*;
+import java.nio.file.Paths;
+
 public class view
 {
+    public view()
+    {
+
+        JFrame frame = new JFrame("Explorer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setIconImage(new ImageIcon(Paths.get(".", "src", "main", "resources", "icons", "256-explorer.png").toString()).getImage());
+        frame.setLayout(new BorderLayout());
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JButton("Hello world"), BorderLayout.NORTH);
+        panel.add(new JButton("Hello World"), BorderLayout.NORTH);
+
+        frame.add(panel, BorderLayout.NORTH);
+        frame.add(new JButton("Hello world"), BorderLayout.WEST);
+        frame.add(new JButton("Hello world"), BorderLayout.SOUTH);
+        frame.add(new JButton("Hello world"), BorderLayout.EAST);
+        frame.add(new JButton("Hello world"), BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
